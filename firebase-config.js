43c2a95696ef0eaac72c7bfd324c277ad14e4eff
garydev0 <gerardlopezgarcia1@gmail.com>
2026-09-14 13:20:18 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,12 +4,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth
 
 // REEMPLAZA ESTO CON LOS DATOS DE TU PROYECTO FIREBASE
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyCFE0T4m16MOyn9JqMujJZZwMJCLen-LgA",
+  authDomain: "elmurazo.firebaseapp.com",
+  projectId: "elmurazo",
+  storageBucket: "elmurazo.firebasestorage.app",
+  messagingSenderId: "722334671597",
+  appId: "1:722334671597:web:4b316572169de9c66262e4",
+  measurementId: "G-P003BX46Z4"
 };
 
 const app = initializeApp(firebaseConfig);
